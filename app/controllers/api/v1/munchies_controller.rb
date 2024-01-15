f42b397307685d9adfdc407b73785f7abc492f2e
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-15 10:45:38 -0700

    make private methods to get information in the controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,8 +2,21 @@ class Api::V1::MunchiesController < ApplicationController
   def index
     destination = params["destination"]
     food = params["food"]
-    facade = RestaurantFacade.new(food, destination).restaurant_info
-    restaurant = Restaurant.new(facade["businesses"].first)
+    forecast_data = get_forecast_data(destination)
+    restaurant_data = get_restaurant_data(food, destination)
+    require 'pry'; binding.pry
+  end
 
+
+  private
+
+  def get_forecast_data(destination)
+    forecast = WeatherFacade.new(destination).formatted_weather_info
+  end
+
+  def get_restaurant_data(food, destination)
+    food = RestaurantFacade.new(food, destination).restaurant_info
+    restaurant = Restaurant.new(food["businesses"].first)
   end
 end
+
