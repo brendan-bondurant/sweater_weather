5c42af6eb5fbd02d78e41a081ab7a2c0eb9feb9a
Brendan Bondurant <bondurant.brendan@gmail.com>
2024-01-15 10:56:55 -0700

    correct formating and response

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -4,7 +4,23 @@ class Api::V1::MunchiesController < ApplicationController
     food = params["food"]
     forecast_data = get_forecast_data(destination)
     restaurant_data = get_restaurant_data(food, destination)
+    # require 'pry'; binding.pry
+
+    response_data = {
+      data: {
+        id: nil,
+        type: 'munchie',
+        attributes: {
+          destination_city: destination,
+          forecast: forecast_data,
+          restaurant: restaurant_data
+        }
+      }
+    }
+    json_response = response_data.to_json
     require 'pry'; binding.pry
+    render json: json_response
+
   end
 
 
@@ -12,6 +28,15 @@ class Api::V1::MunchiesController < ApplicationController
 
   def get_forecast_data(destination)
     forecast = WeatherFacade.new(destination).formatted_weather_info
+    summary = forecast.dig(:data, :attributes, :current_weather, :condition)
+    temperature = forecast.dig(:data, :attributes, :current_weather, :temperature)
+
+    response_data = {
+      summary: summary,
+      temperature: temperature
+    }
+
+    return response_data
   end
 
   def get_restaurant_data(food, destination)
